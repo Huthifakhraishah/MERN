39a93024608dda5869e1bf053c8192f65a1dbe40
huthifa <huthifakhraishah98@gmail.com>
2021-07-13 14:03:38 +0300

    first commit

---
+++ fundementals/ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+    constructor(name, health=0, speed=3, strength=3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName(){
+        console.log("The ninja name" + this.name);
+    }
+
+    showStats(){
+        this.sayName();
+        console.log("Health: " + this.health);
+        console.log("Speed: " + this.speed);
+        console.log("strength: " + this.strength);
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
