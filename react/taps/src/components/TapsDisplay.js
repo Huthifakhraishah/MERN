34e9a0d35cf934488c1a0b0b49fcd5c32d9fbd80
huthifa <huthifakhraishah98@gmail.com>
2021-07-27 23:58:00 +0300

    latest assignment

---
+++ react/taps/src/components/TapsDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TapsDisplay = (props) => {
+    return (
+        <div>
+            <h3>{props.content}</h3>
+        </div>
+    )
+}
+export default TapsDisplay
