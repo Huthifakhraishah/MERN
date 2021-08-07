374b97370e9866628cb04682f942dc830ec67dd5
huthifa <huthifakhraishah98@gmail.com>
2021-08-08 00:13:39 +0300

    chatt

---
+++ full stack/creating-the-handshake/client/src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Chat = () => {
+
+    return (
+        <h1>how u doin</h1>
+    )
+}
+
+export default Chat;
---
+++ full stack/creating-the-handshake/server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+mongoose.connect("mongodb://localhost/chatapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Established a connection'))
+.catch((err) =>{
+    console.log('Something went wrong connecting to the database'), err
+});
